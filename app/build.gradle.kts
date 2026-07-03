12fdb0a18d14ffaa31eb4f5e7eca917c0ae17c3b
Ariel Cruz Susaño <acruzsu@aduana.gob.bo>
2026-07-03 00:45:18 -0400

    fix: versión sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.univalle.practica1"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
